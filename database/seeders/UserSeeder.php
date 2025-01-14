8d21a302e39dcf32eaa804a165432bb27aaa5adf
Agus <agusalfandi8@gmail.com>
2025-01-14 19:22:31 +0700

    add firtur role users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,5 +14,21 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::factory()->count(5)->create();
+
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'superadmin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'),
+            'role' => 'superadmin',
+        ]);
+
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'),
+            'role' => 'admin',
+        ]);
     }
 }
